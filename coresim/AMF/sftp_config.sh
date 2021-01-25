f44baace428c7eec204102a147dac88f2a21dfee
PRAVEEN_SPR <praveen.velugubantla123@gmail.com>
2021-01-25 12:56:45 +0530

    5G-core-nf-simulator code
    The pylint linter reports LOTS of critical errors
    Until I got some feedback from the developer, I created a pylintrc,
    in order to allow a first merge despite of all the errors.
    
    But the quality of the code does not match the integration criteria.
    All the C* and E* errors indicated in the .pylintrc file shall be corrected
    in order to match integration criteria
    
    Topic to be discussed during the Integraiton meeting
    - option 1: we abandon the patch but the repo is empty
    - option 2: we merge with the exceptions
    - option 3: errors fixed then merged
    
    of course 3 is the priviledged option.
    but if not possible, we need to take a decision
    
    Issue-ID: INT-1843
    
    Change-Id: Ie63ccfaec6be04d47cd12e000d386389e4110a1d
    Signed-off-by: PRAVEEN_SPR <praveen.velugubantla123@gmail.com>
    Signed-off-by: morganrol <morgan.richomme@orange.com>

---
+++ coresim/AMF/sftp_config.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+echo "Started executing the sftp bash file"
+
+echo "Creating the sftp user & group with a directory"
+
+mkdir -p /data
+
+chmod 701 /data
+
+groupadd sftp_users
+
+useradd -g sftp_users -d /pm_directory -s /sbin/nologin admin
+
+echo "admin:admin" | chpasswd
+
+mkdir -p /data/admin/pm_directory
+
+chown -R root:sftp_users /data/admin
+
+chown -R admin:sftp_users /data/admin/pm_directory
+
+echo "Now copying the sshd config"
+
+cp sshd_config /etc/ssh/sshd_config
+
+echo "After copying sshd,Restarting the service ssh"
+
+service ssh restart
+
+#Copy temporary config-file into the container
+
+#mkdir /etc/config
+
+#mkdir /etc/config/amf-conf
+
+#mv ./config/amf-conf/supportedNssai.json /etc/config/
+
+#mv ./config/amf-conf/dcae_collector_ip.txt /etc/config/amf-conf/
+
+#mv ./config/amf-conf/dcae_collector_port.txt /etc/config/amf-conf/
+
+#mv ./config/amf-conf/granularity_period.txt /etc/config/amf-conf/
+
